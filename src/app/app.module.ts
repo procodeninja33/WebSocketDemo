1bf330206ebf3c992f603a3676e81f709ac271dd
Nizamuddin <pro.codeninja33@gmail.com>
2019-06-17 10:47:04 +0530

    admin dashboard live records, online offlice name remove to add green dots

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
